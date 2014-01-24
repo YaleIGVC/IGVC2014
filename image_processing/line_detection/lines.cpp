7c59dbdaab171d7f3fbb302eb7a4c6d4e784b6c3
Kevin Abbott <kgabbott@att.net>
2014-01-23 21:37:10 -0500

    added line detection and visual representation

--- image_processing/line_detection/lines.cpp
+++ image_processing/line_detection/lines.cpp
@@ -11,10 +11,11 @@ using namespace cv;
 
 /// Global variables
 
-Mat src, blueSrc, medianImg, thresholdImg;
+Mat src, blueSrc, medianImg, thresholdImg, lineImg;
 Mat channel[3];
 int thresholdValue = 200;
 int maxval = 255;
+vector<Vec4i> lines;
 
 /** @function main */
 int main( int argc, char** argv )
@@ -30,7 +31,17 @@ int main( int argc, char** argv )
     threshold(medianImg, thresholdImg, thresholdValue, 255, THRESH_BINARY);
     //cvtColor(medianImg, hsvImg, CV_BGR2HSV);
     //inRange(hsvImg, Scalar(Hlow, Slow, Vlow), Scalar(Hhigh, Shigh, Vhigh), inRangeImg);
-    imshow("Lines", thresholdImg);
+    HoughLinesP( thresholdImg, lines, 1, CV_PI/180, 80, 30, 10 );
+    thresholdImg.copyTo(lineImg);
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        line( lineImg, Point(lines[i][0], lines[i][1]),
+            Point(lines[i][2], lines[i][3]), Scalar(122, 122, 122), 3, 8 );
+    }
+    namedWindow( "Detected Lines", 1 );
+    imshow( "Detected Lines", lineImg );
+    namedWindow( "Lines", 1 );
+    imshow( "Lines", thresholdImg );
 
     waitKey(0);
 
