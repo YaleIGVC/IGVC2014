ccde2061d81be4ba8f1322ea88d688a6f6b5d1c1
Alejandro Carrillo <aecarrillo594@gmail.com>
2014-01-24 17:31:33 -0500

    Add joystick checker

--- joystick_ctrl/joystick_ctrl.py
+++ joystick_ctrl/joystick_ctrl.py
@@ -1,23 +1,7 @@
 import pygame
 import sys
-import curses
 
-stdscr=curses.initscr()
-stdscr.keypad(1)
-stdscr.nodelay(1)
-stdscr.leaveok(1)
-curses.noecho()
-curses.cbreak()
-
-stdscr=curses.initscr()
-stdscr.keypad(1)
-stdscr.nodelay(1)
-stdscr.leaveok(1)
-
-pad = curses.newpad(200, 200)
-
-axes=['','','','','','']
-
+axes=[0,0,0,0,0,0]
 
 pygame.init()
 
@@ -26,22 +10,14 @@ pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-while 1:
-    if stdscr.getch()==ord('q'):
+while True:
+    c = sys.stdin.read(1)
+    if (c == 'q'):
         break
-    elif stdscr.getch()==ord('r'):
+    if c == 'r':
+        pygame.event.get()
         for i in range(6):
-            axes[i] = joystick.get_axis(i)[10:]
-    pad.addstr(0,0, "0:");pad.addstr(0,50,str(axes[0])+"   ")
-    pad.addstr(1,0, "1:");pad.addstr(0,50,str(axes[1])+"   ")
-    pad.addstr(2,0, "2:");pad.addstr(0,50,str(axes[2])+"   ")
-    pad.addstr(3,0, "3:");pad.addstr(0,50,str(axes[3])+"   ")
-    pad.addstr(4,0, "4:");pad.addstr(0,50,str(axes[4])+"   ")
-    pad.addstr(5,0, "5:");pad.addstr(0,50,str(axes[5])+"   ")
-    stdscr.refresh()
-    pad.refresh( 0,0, 0,0, 200,200)
-    #print "RPM\t",rpm,"\tTemp\t",temp,"\tLow Battery\t",battlow,"\tFuelIn\t",fuelin,"\tThrottle\t",throttlein,"\tBMS Fault\t",bmsfault,"\tClutch\t",clutch,"\tAssist In\t",assist,"\tBrake In\t",brake
+            axes[i] = joystick.get_axis(i)
+        print axes
 
 # Quit!
-curses.nocbreak(); stdscr.keypad(0); curses.echo()
-curses.endwin()
