cd8391f78f36d9da2507d11a1ea7b3cbeb51fb1c
Alejandro Carrillo <aecarrillo594@gmail.com>
2014-01-24 23:05:25 -0500

    Update joystick control hack

--- joystick_ctrl/joystick_ctrl_hack.py
+++ joystick_ctrl/joystick_ctrl_hack.py
@@ -1,10 +1,17 @@
-mport sys
-pipe = open('/dev/input/js1','r')
+import sys
+pipe = open('/dev/input/js0','r')
 action = []
 spacing = 0
-while True:
-	for char in pipe.read(1):
-		action += [character]
-		if len(action) == 8:
-			for byte in action:
-				sys.stdout.write('%02X ' % s
+while 1:
+        for character in pipe.read(1):
+                action += [character]
+                if len(action) == 8:
+                        for byte in action:
+                                sys.stdout.write('%02X ' % ord(byte))
+                        spacing += 1
+                        if spacing == 2:
+                                sys.stdout.write('\n')
+                                spacing = 0
+                        sys.stdout.write('\n')
+                        sys.stdout.flush()
+                        action = []
