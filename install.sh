6ecae0a80f431d7e217af796854710e7c5724369
Alejandro Carrillo <aecarrillo594@gmail.com>
2014-02-22 18:01:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sudo apt-get install ros-hydro-hector-mapping
-sudo apt-get install ros-hydro-hector-slam
-sudo apt-get install ros-hydro-hokuyo-node
+sudo apt-get install ros-hydro-hector-mapping ros-hydro-hector-slam ros-hydro-hokuyo-node
+
+catkindirpath="~/catkin_workspace/src"
+mkdir -p $catkindirpath
+cd $catkindirpath
+
+catkin_init_workspace
+catkin_make
+git clone https://github.com/segwayrmp/segway-rmp-ros-pkg.git
+cd segway-rmp-ros-pkg
