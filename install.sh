49e3357f78f37ad27536489b957f1a7f50e323cf
Alejandro Carrillo <aecarrillo594@gmail.com>
2014-02-22 18:07:55 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo apt-get install ros-hydro-hector-mapping ros-hydro-hector-slam ros-hydro-hokuyo-node
+sudo apt-get install ros-hydro-hector-mapping ros-hydro-hector-slam ros-hydro-hokuyo-node ros-hydro-serial ros-hydro-libsegwayrmp
 
 catkindirpath="~/catkin_workspace/src"
+username=`whoami`
+
+# Add ourselves to dialout so that we don't need to chmod USB devices
+sudo usermod -aG dialout $username
+
 mkdir -p $catkindirpath
 cd $catkindirpath
 
