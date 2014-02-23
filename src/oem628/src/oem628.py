ec7f58af21e035499da8c7cf60b54578949cd39f
Inve Dev Machine <info@yaleigvc.com>
2014-02-23 16:44:25 -0500

    Load in ttyUSB* dynamically

--- src/oem628/src/oem628.py
+++ src/oem628/src/oem628.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 import rospy
 import serial
+import subprocess
 from std_msgs.msg import String
 
 # Initialize Serial comms
 class Gps:
     def __init__(self):
+        self.pub = rospy.Publisher('pos', String)
+        rospy.init_node('oem628')
+
         self.BAUD = 9600
-        self.ser = serial.Serial('/dev/ttyUSB0',9600)
+        p = subprocess.Popen(['./getusb.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        usb = out[:-1]
+        rospy.loginfo("Detected USB is /dev"+usb)
+        self.ser = serial.Serial('/dev/'+usb,9600)
 
         # Start reporting GPS position
         self.ser.write("log bestposa ontime 1\n")
 
     def talker(self):
-        pub = rospy.Publisher('pos', String)
-        rospy.init_node('oem628')
         while not rospy.is_shutdown():
             rawline = self.ser.readline()
             line = rawline.split(",")
@@ -23,7 +29,7 @@ class Gps:
                 lon = line[12]
                 str = lat + " " + lon
                 rospy.loginfo(str)
-                pub.publish(String(str))
+                self.pub.publish(String(str))
             rospy.sleep(0.5)
 
 
