ab19fe0c327079e0c1b5718f4191ca0daabf553f
Inve Dev Machine <info@yaleigvc.com>
2014-02-23 15:39:38 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sudo apt-get install ros-hydro-hector-mapping ros-hydro-hector-slam ros-hydro-hokuyo-node ros-hydro-serial ros-hydro-libsegwayrmp
+sudo apt-get install ros-hydro-hector-mapping ros-hydro-hector-slam ros-hydro-hokuyo-node ros-hydro-serial ros-hydro-libsegwayrmp nautilus-dropbox python-pip
+sudo pip install pyserial
+
+echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
+echo "source ~/catkin_workspace/devel/setup.bash" >> ~/.bashrc
+echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/catkin_workspace" >> ~/.bashrc
 
 catkindirpath="~/catkin_workspace/src"
 username=`whoami`
@@ -11,6 +16,8 @@ mkdir -p $catkindirpath
 cd $catkindirpath
 
 catkin_init_workspace
+cd ..
 catkin_make
+cd src
 git clone https://github.com/segwayrmp/segway-rmp-ros-pkg.git
 cd segway-rmp-ros-pkg
