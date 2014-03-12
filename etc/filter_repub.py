4bb975acbf72238f819cd9bc186b4d9be8cdda56
info@yaleigvc.com <Alejandro Carrillo>
2014-03-12 01:37:49 -0400

    Use euler_from_quaternion in filter_repub script

--- etc/filter_repub.py
+++ etc/filter_repub.py
@@ -1,40 +1,38 @@
 import rospy
 import tf
-from nav_msgs import Odometry
+from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 
-class Repub:
-    def __init__(self):
-        rospy.init_node('odom_repub',anonymous=True)
-        self.pub = rospy.Publisher("/odom_repub", Imu)
-        rospy.Subscriber("/segway_rmp_node/odom", Odometry, self.callback)
+def callback(msg_in):
+    print "wat"
+    rospy.loginfo("In callback!!!!")
+    a  = msg_in.pose.pose.orientation.x
+    i  = msg_in.pose.pose.orientation.y
+    j  = msg_in.pose.pose.orientation.z
+    k  = msg_in.pose.pose.orientation.w
 
-    def callback(self,msg_in):
-        a  = msg_in.pose.pose.orientation.x
-        i  = msg_in.pose.pose.orientation.y
-        j  = msg_in.pose.pose.orientation.z
-        k  = msg_in.pose.pose.orientation.w
+    quaternion = (a,i,j,k)
+    q = tf.transformations.euler_from_quaternion(quaternion)
 
-        q = tf.transformations.euler_from_quaternion(a, i, j,k)
+    msg_imu = Imu()
+    msg_imu.header.stamp          = msg_in.header.stamp
+    msg_imu.header.frame_id       = msg_in.header.frame_id
 
-        msg_imu = Imu()
-        msg_imu.header.stamp          = msg_in.header.stamp
-        msg_imu.header.frame_id       = msg_in.header.frame_id
+    # HACK: Use angular_velocity field to represent orientation!!!
+    msg_imu.angular_velocity.x    = q[0]
+    msg_imu.angular_velocity.y    = q[1]
+    msg_imu.angular_velocity.z    = q[2]
 
-        # HACK: Use angular_velocity field to represent orientation!!!
-        msg_imu.angular_velocity.x    = q[0]
-        msg_imu.angular_velocity.y    = q[1]
-        msg_imu.angular_velocity.z    = q[2]
-
-        msg_imu.linear_acceleration.x = msg_in.twist.twist.linear.x
-        msg_imu.linear_acceleration.y = msg_in.twist.twist.linear.y
-        msg_imu.linear_acceleration.z = msg_in.twist.twist.linear.z
-        self.pub.publish(msg_imu)
-
-    def go(self):
-        rospy.spin()
+    msg_imu.linear_acceleration.x = msg_in.twist.twist.linear.x
+    msg_imu.linear_acceleration.y = msg_in.twist.twist.linear.y
+    msg_imu.linear_acceleration.z = msg_in.twist.twist.linear.z
+    global pub
+    pub.publish(msg_imu)
 
 if __name__=='__main__':
-    r = Repub()
-    r.go()
-
+    rospy.init_node('odom_repub',anonymous=True)
+    global pub
+    pub = rospy.Publisher("/odom_repub", Imu)
+    rospy.Subscriber("/segway_rmp_node/odom", Odometry, callback)
+    rospy.loginfo("init")
+    rospy.spin()
