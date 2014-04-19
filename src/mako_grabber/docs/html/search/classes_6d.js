9291eeafb9fc78cd23c0a2cfbce891f2829cfa49
info@yaleigvc.com <Alejandro Carrillo>
2014-04-19 11:50:46 -0400

    merge

---
+++ src/mako_grabber/docs/html/search/classes_6d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['makocamera',['MakoCamera',['../classMakoCamera.html',1,'']]]
+];
---
+++ src/mako_grabber/src/MakoGrabber.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "MakoCamera.h"
+
+using namespace std;
+
+int main ( int argc, char *argv[] );
+
+int main( int argc, char *argv[]) {
+    return 0;
+}
