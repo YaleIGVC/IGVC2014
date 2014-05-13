27718035d91092b92768472a6fd4dd6252122c0e
info@yaleigvc.com <Alejandro Carrillo>
2014-05-12 22:56:14 -0400

    Script to grab rqt frame

--- src/rqt_single_image_grabber/src/convert.py
+++ src/rqt_single_image_grabber/src/convert.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from datetime import datetime
+
 if __name__ == '__main__':
-	print "Hello world"
+    rospy.init_node("rqt_single_image_grabber")
+    messagein = rospy.wait_for_message('/raw_image', Image)
+
+    bridge = CvBridge()
+    print "Attempting to grab image"
+    try:
+        frame = bridge.imgmsg_to_cv2(messagein, "bgr8")
+    except CvBridgeError, e:
+        print e
+    cv2.imwrite(datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '-grabbed.jpg', frame)
+    print "Success!"
