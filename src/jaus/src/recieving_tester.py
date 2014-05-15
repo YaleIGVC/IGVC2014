756a2fe3a75379e309062b83e3f4c185c58ffb51
jasonsbrooks <jason.brooks@yale.edu>
2014-05-14 22:30:28 -0400

    Modifying receiver

--- src/jaus/src/recieving_tester.py
+++ src/jaus/src/recieving_tester.py
@@ -1,7 +1,7 @@
 import socket
  
 My_UDP_IP = ""
-JTC_UDP_IP = "192.168.1.42"
+JTC_UDP_IP = "192.168.1.127"
 UDP_PORT = 3794
  
 sock = socket.socket(socket.AF_INET, # Internet
@@ -9,8 +9,13 @@ sock = socket.socket(socket.AF_INET, # Internet
 
 sock.bind((My_UDP_IP, UDP_PORT))
 
+def jaus_switch_main(cmd):
+	if cmd == 'hello_world':
+		print "HEY BABEE"
+
 while True:
     command, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    jaus_switch_main(command)
     print "JTC: ", command
 
     response = raw_input("Robot: ")
