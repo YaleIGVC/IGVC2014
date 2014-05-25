f8ab7e716d7d1b229d9eeac8b3c053fca221732b
Jason Brooks <info@yaleigvc.com>
2014-05-24 22:44:44 -0400

    Use start.sh script to invoke launch file

--- launch_files/start.sh
+++ launch_files/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+source ~/.bashrc
 # Modify this line to change what file to launch
-LAUNCHFILE_NAME=test.launch
+LAUNCHFILE_NAME=~/IGVC2014/launch_files/test.launch
 
 # Get environment files from getsusbs
-dir=`roscd getusbs`
-file=$dir/src/getusbs.sh
+file=~/IGVC2014/src/getusbs/src/getusbs.sh
 source $file
 
 roslaunch $LAUNCHFILE_NAME
