08d76aac9066c417d881499a70c490f1d5b0aac3
Jason Brooks <info@yaleigvc.com>
2014-05-24 22:24:34 -0400

    Add node 'getusbs' that creates environment variables for /dev/ttyX devices

---
+++ src/getusbs/src/getusbs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+segway_idProduct=`lsusb | grep Segway | cut -f6 -d" " | cut -f2 -d":"`
+segway_usbno=`dmesg | grep $segway_idProduct | grep -oe '[0-9]-[0-9]'`
+SEGWAY_TTY=`dmesg | grep $segway_usbno | grep -oe 'ttyUSB[0-9]'`
+
+vectornav_idProduct=`lsusb | grep FT232 | cut -f6 -d" " | cut -f2 -d":"`
+vectornav_usbno=`dmesg | grep $vectornav_idProduct | grep -oe '[0-9]-[0-9]'`
+VECTORNAV_TTY=`dmesg | grep $vectornav_usbno | grep -oe 'ttyUSB[0-9]'`
+
+# Commented out because we do not yet have a GPS installed
+#gps_idProduct=`lsusb | grep PL2303 | cut -f6 -d" " | cut -f2 -d":"`
+#gps_usbno=`dmesg | grep $gps_idProduct | grep idProduct | grep -oe '[0-9]-[0-9]'`
+#GPS_TTY=`dmesg | grep $gps_usbno | grep ttyUSB | cut -f13 -d" "`
+
+export segway_serial_port=/dev/$SEGWAY_TTY
+export imu_serial_port=/dev/$VECTORNAV_TTY
+export gps_serial_port=/dev/$GPS_TTY
