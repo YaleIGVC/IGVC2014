0f8fecf20882cdb74b7e5b8ea21e03587187cc9e
Soham Sankaran <soham.sankaran@gmail.com>
2014-05-29 16:56:09 -0400

    fhg

--- src/lanelord/src/ll_wtf_circle.py
+++ src/lanelord/src/ll_wtf_circle.py
@@ -86,7 +86,7 @@ class lanelord():
             cimg = cv2.cvtColor(ximg,cv2.COLOR_GRAY2BGR)
             circles = cv2.HoughCircles(ximg,cv2.cv.CV_HOUGH_GRADIENT,1,20,
                             param1=50,param2=30,minRadius=0,maxRadius=100)
-            #circles = np.uint16(np.around(circles))
+            circles = np.uint16(np.around(circles))
             print circles
             self.cxcoord = circles[0][0][0]
             self.cycoord = circles[0][0][1]
