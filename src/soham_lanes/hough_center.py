fb92950181c16a55c12584b60103977638bbc644
Soham Sankaran <soham.sankaran@gmail.com>
2014-05-29 02:24:55 -0400

    commit centering program

---
+++ src/soham_lanes/hough_center.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('itt5.jpg',0)
+#img = cv2.resize(img, (img.shape[1] / 2, img.shape[0] / 2))
+img = cv2.medianBlur(img,51)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+
+
+circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=180,maxRadius=250)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+    print "circle coordinates: " + str((i[0],i[1]))
+
+cv2.imshow('detected circles',cimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
