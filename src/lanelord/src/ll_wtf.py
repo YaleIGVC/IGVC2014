9b6dbc41e06ad83c398fc787bf0d1b798c89d68f
Inve Dev Machine <info@yaleigvc.com>
2014-06-04 04:01:46 -0400

    Increase blur in lanelord

--- src/lanelord/src/ll_wtf.py
+++ src/lanelord/src/ll_wtf.py
@@ -73,7 +73,7 @@ class lanelord():
         img = cv2.resize(img, (img.shape[1] / 8, img.shape[0] / 8))
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-        detected_edges = cv2.GaussianBlur(gray,(11,11),0)
+        detected_edges = cv2.GaussianBlur(gray,(15,15),0)
         detected_edges = cv2.Canny(detected_edges,lowThreshold,lowThreshold*ratio,apertureSize = kernel_size)
         dst = cv2.bitwise_and(img,img,mask = detected_edges)  # just add some colours to edges from original image.
 
